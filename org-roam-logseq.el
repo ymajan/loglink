6f9a0b7a5e9d25c163882dae847d336e7b7c5f31
ymajan <jaynam@ymajan.pw>
2024-09-22 18:07:42 -0400

    updates

--- org-roam-logseq.el
+++ org-roam-logseq.el
@@ -3,13 +3,16 @@
 (require 'cl-lib)
 (require 'org)
 (require 'org-roam)
+(require 'f)
+;;(use-package f
+;;  :ensure t)
 ;; logseq folder same as org-roam directory
 (defvar ymajan/logseq-folder org-roam-directory)
 
 (defvar ymajan/logseq-pages (f-expand (f-join ymajan/logseq-folder "pages")))
-;;(defvar ymajan/logseq-journals (f-expand (f-join ymajan/logseq-folder "journals")))
+(defvar ymajan/logseq-journals (f-expand (f-join ymajan/logseq-folder "journals")))
 
 ;; exclude files in logseq/bak/ folder via regex
-;;(defvar ymajan/logseq-exclude-pattern (string-join (list "^" (file-truename ymajan/logseq-folder) "/logseq/bak/.*$")))
+(defvar ymajan/logseq-exclude-pattern (string-join (list "^" (file-truename ymajan/logseq-folder) "/logseq/bak/.*$")))
 
-;;(defun ymajan/logseq-journal-p (file) (string-match-p (concat "^" ymajan/logseq-journals) file))
+(defun ymajan/logseq-journal-p (file) (string-match-p (concat "^" ymajan/logseq-journals) file))
